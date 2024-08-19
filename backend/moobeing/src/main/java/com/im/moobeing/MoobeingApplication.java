28ce4e2abc1ada9e70637f0afcfc6f6af08c030d
chwangmin <ckm7907cb@gmail.com>
2024-08-20 06:33:33 +0900

    Init

---
+++ backend/moobeing/src/main/java/com/im/moobeing/MoobeingApplication.java
@@ -0,0 +1,13 @@
+package com.im.moobeing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoobeingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoobeingApplication.class, args);
+	}
+
+}
