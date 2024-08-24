c4b204ee99ed060c76f97dfe26c64a8ae954df7c
chwangmin <ckm7907cb@gmail.com>
2024-08-24 10:51:23 +0900

    fix: quiz 앞에 api 삭제

--- backend/moobeing/src/main/java/com/im/moobeing/domain/quiz/controller/QuizController.java
+++ backend/moobeing/src/main/java/com/im/moobeing/domain/quiz/controller/QuizController.java
@@ -1,18 +1,5 @@
 package com.im.moobeing.domain.quiz.controller;
 
-import java.util.List;
-
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.ErrorResponse;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.im.moobeing.domain.member.entity.Member;
 import com.im.moobeing.domain.quiz.dto.request.QuizAnswerRequest;
 import com.im.moobeing.domain.quiz.dto.response.QuizAnswerResponse;
@@ -20,16 +7,22 @@ import com.im.moobeing.domain.quiz.dto.response.QuizColdResponse;
 import com.im.moobeing.domain.quiz.dto.response.QuizDetailResponse;
 import com.im.moobeing.domain.quiz.dto.response.QuizResponse;
 import com.im.moobeing.domain.quiz.service.QuizService;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/quiz")
+@RequestMapping("/quiz")
 @RequiredArgsConstructor
 public class QuizController {
 	private final QuizService quizService;
