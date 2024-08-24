73a986ed968565dfa47f5f5ac64c4b4ad0bc648e
chwangmin <ckm7907cb@gmail.com>
2024-08-24 20:29:33 +0900

    fix: expense /api 부분 삭제

--- backend/moobeing/src/main/java/com/im/moobeing/domain/expense/controller/ExpenseController.java
+++ backend/moobeing/src/main/java/com/im/moobeing/domain/expense/controller/ExpenseController.java
@@ -1,7 +1,15 @@
 package com.im.moobeing.domain.expense.controller;
 
-import java.util.List;
-
+import com.im.moobeing.domain.expense.dto.response.ExpenseCategoryResponse;
+import com.im.moobeing.domain.expense.dto.response.ExpenseDateResponse;
+import com.im.moobeing.domain.expense.service.ExpenseService;
+import com.im.moobeing.domain.member.entity.Member;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -10,20 +18,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.im.moobeing.domain.expense.dto.response.ExpenseCategoryResponse;
-import com.im.moobeing.domain.expense.dto.response.ExpenseDateResponse;
-import com.im.moobeing.domain.expense.service.ExpenseService;
-import com.im.moobeing.domain.member.entity.Member;
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/expense")
+@RequestMapping("/expense")
 @RequiredArgsConstructor
 public class ExpenseController {
 	private final ExpenseService expenseService;
