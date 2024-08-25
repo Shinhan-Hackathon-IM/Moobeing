e54b2126e48951dcfda49aaa287e3ebcfbe37129
chwangmin <ckm7907cb@gmail.com>
2024-08-25 16:09:24 +0900

    fix: 초기 data 실제 사용할 데이터로 변경

--- backend/moobeing/src/main/resources/data.sql
+++ backend/moobeing/src/main/resources/data.sql
@@ -1,12 +1,13 @@
--- radish 테이블에 데이터 삽입
-INSERT IGNORE INTO radish (radish_id, created_date, modified_date, radish_image, radish_name, radish_rank) VALUES
-(1, '2024-08-23 12:00:00', '2024-08-23 12:00:00', 'radish_image1.jpg', 'Radish A', 'A1'),
-(2, '2024-08-23 12:05:00', '2024-08-23 12:05:00', 'radish_image2.jpg', 'Radish B', 'B1'),
-(3, '2024-08-23 12:10:00', '2024-08-23 12:10:00', 'radish_image3.jpg', 'Radish C', 'C1'),
-(4, '2024-08-23 12:15:00', '2024-08-23 12:15:00', 'radish_image4.jpg', 'Radish D', 'D1'),
-(5, '2024-08-23 12:20:00', '2024-08-23 12:20:00', 'radish_image5.jpg', 'Radish E', 'E1'),
-(6, '2024-08-23 12:25:00', '2024-08-23 12:25:00', 'radish_image6.jpg', 'Radish F', 'F1'),
-(7, '2024-08-23 12:30:00', '2024-08-23 12:30:00', 'radish_image7.jpg', 'Radish G', 'G1'),
-(8, '2024-08-23 12:35:00', '2024-08-23 12:35:00', 'radish_image8.jpg', 'Radish H', 'H1'),
-(9, '2024-08-23 12:40:00', '2024-08-23 12:40:00', 'radish_image9.jpg', 'Radish I', 'I1'),
-(10, '2024-08-23 12:45:00', '2024-08-23 12:45:00', 'radish_image10.jpg', 'Radish J', 'J1');
+INSERT IGNORE INTO radish (radish_id, radish_name, radish_image_url, radish_rank, created_date, modified_date) VALUES
+(1, 'basicRad', 'https://github.com/user-attachments/assets/19eff918-f0cd-4f7c-b56b-ddf5069749b9', 'B', '2024-08-23 12:00:00', '2024-08-23 12:00:00'),
+(2, 'aniRad', 'https://github.com/user-attachments/assets/989d6ac6-39d1-4f24-85aa-ac2c6deedc4c', 'B', '2024-08-23 12:05:00', '2024-08-23 12:05:00'),
+(3, 'babyRad', 'https://github.com/user-attachments/assets/67db296c-70b7-422c-bc1f-8fab92414dc6', 'B', '2024-08-23 12:10:00', '2024-08-23 12:10:00'),
+(4, 'blushRad', 'https://github.com/user-attachments/assets/29766555-5bb9-4848-88c3-79039ecaa61a', 'B', '2024-08-23 12:15:00', '2024-08-23 12:15:00'),
+(5, 'flippedRad', 'https://github.com/user-attachments/assets/55610e6e-e7a0-41f9-b81a-d41ce79bdbe8', 'B', '2024-08-23 12:20:00', '2024-08-23 12:20:00'),
+(6, 'hairlotRad', 'https://github.com/user-attachments/assets/474185cd-9469-4394-a0b9-48fd189b66d4', 'B', '2024-08-23 12:25:00', '2024-08-23 12:25:00'),
+(7, 'musinsaRad', 'https://github.com/user-attachments/assets/5a28373c-c434-4d2b-8df9-27dc48c6df05', 'B', '2024-08-23 12:30:00', '2024-08-23 12:30:00'),
+(8, 'pot', 'https://github.com/user-attachments/assets/f376d8fb-ff80-4a20-b666-a95413e96b30', 'B', '2024-08-23 12:35:00', '2024-08-23 12:35:00'),
+(9, 'rainbowRad', 'https://github.com/user-attachments/assets/28b348f4-cd9e-4a65-9390-49bec79af10f', 'B', '2024-08-23 12:40:00', '2024-08-23 12:40:00'),
+(10, 'soil', 'https://github.com/user-attachments/assets/7bcff5a8-7233-4077-a9aa-cf73a53a9220', 'B', '2024-08-23 12:45:00', '2024-08-23 12:45:00'),
+(11, 'vacationRad', 'https://github.com/user-attachments/assets/0bd57d4a-f59f-4cdc-ba71-5ce07a37bf9e', 'B', '2024-08-23 12:50:00', '2024-08-23 12:50:00'),
+(12, 'weightRad', 'https://github.com/user-attachments/assets/eb74b691-4b58-4532-bf7d-087bc944a844', 'B', '2024-08-23 12:55:00', '2024-08-23 12:55:00');
