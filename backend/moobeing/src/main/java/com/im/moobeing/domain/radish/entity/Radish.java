f89ead622aa6bc8d96e6b979cac4dcade4e9cb22
chwangmin <ckm7907cb@gmail.com>
2024-08-25 15:21:55 +0900

    fix: radish entity 탭 없애기

--- backend/moobeing/src/main/java/com/im/moobeing/domain/radish/entity/Radish.java
+++ backend/moobeing/src/main/java/com/im/moobeing/domain/radish/entity/Radish.java
@@ -1,43 +1,41 @@
-    package com.im.moobeing.domain.radish.entity;
+package com.im.moobeing.domain.radish.entity;
 
-    import com.im.moobeing.domain.member.entity.MemberRadish;
-    import com.im.moobeing.global.entity.BaseTimeEntity;
-    import jakarta.persistence.*;
-    import lombok.AccessLevel;
-    import lombok.Builder;
-    import lombok.Getter;
-    import lombok.NoArgsConstructor;
+import com.im.moobeing.domain.member.entity.MemberRadish;
+import com.im.moobeing.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.util.List;
 
-    import java.util.List;
+@Entity(name = "radish")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Radish extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "radish_id")
+    private Long id;
 
+    @Column(name = "radish_name", nullable = false, length = 200)
+    private String radishName;
 
-    @Entity(name = "radish")
-    @Getter
-    @NoArgsConstructor(access = AccessLevel.PROTECTED)
-    public class Radish extends BaseTimeEntity {
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        @Column(name = "radish_id")
-        private Long id;
+    @Column(name = "radish_image_url", nullable = true, length = 200)
+    private String radishImageUrl;
 
-        @Column(name = "radish_name", nullable = false, length = 200)
-        private String radishName;
+    @Column(name = "radish_rank", nullable = true, length = 40)
+    private String radishRank;
 
-        @Column(name = "radish_image_url", nullable = true, length = 200)
-        private String radishImageUrl;
+    @OneToMany(mappedBy = "radish")
+    private List<MemberRadish> memberRadishes;
 
-        @Column(name = "radish_rank", nullable = true, length = 40)
-        private String radishRank;
-
-        @OneToMany(mappedBy = "radish")
-        private List<MemberRadish> memberRadishes;
-
-        @Builder
-        public Radish(Long id, String radishName, String radishImageUrl, String radishRank, List<MemberRadish> memberRadishes) {
-            this.id = id;
-            this.radishName = radishName;
-            this.radishImageUrl = radishImageUrl;
-            this.radishRank = radishRank;
-            this.memberRadishes = memberRadishes;
-        }
+    @Builder
+    public Radish(Long id, String radishName, String radishImageUrl, String radishRank, List<MemberRadish> memberRadishes) {
+        this.id = id;
+        this.radishName = radishName;
+        this.radishImageUrl = radishImageUrl;
+        this.radishRank = radishRank;
+        this.memberRadishes = memberRadishes;
     }
+}
