0ec56d8b3269f3fac60c75593d0f332ac3cf6ae5
chwangmin <ckm7907cb@gmail.com>
2024-08-25 14:30:23 +0900

    feat: 맴버 create dto 로직 설정

--- backend/moobeing/src/main/java/com/im/moobeing/domain/member/dto/request/MemberCreateRequest.java
+++ backend/moobeing/src/main/java/com/im/moobeing/domain/member/dto/request/MemberCreateRequest.java
@@ -1,5 +1,7 @@
 package com.im.moobeing.domain.member.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,9 +10,19 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Getter
 public class MemberCreateRequest {
+    @Email(message = "유효한 이메일 형식이 아닙니다.")
+    @NotBlank(message = "이메일은 필수 입력 항목입니다.")
     private String email;
+
+    @NotBlank(message = "비밀번호는 필수 입력 항목입니다.")
     private String password;
+
+    @NotBlank(message = "이름은 필수 입력 항목입니다.")
     private String name;
+
+    @NotBlank(message = "성별은 필수 입력 항목입니다.")
     private String gender;
+
+    @NotBlank(message = "생일은 필수 입력 항목입니다.")
     private String birthday;
 }
