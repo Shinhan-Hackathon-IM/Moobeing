3a27b702e469e13fb218c688ac376563290386b0
chwangmin <ckm7907cb@gmail.com>
2024-08-26 00:22:29 +0900

    fix: 필요없는 credit entity 관련 삭제

--- backend/moobeing/src/main/java/com/im/moobeing/domain/credit/repository/CreditRepository.java
+++
@@ -1,7 +0,0 @@
-package com.im.moobeing.domain.credit.repository;
-
-import com.im.moobeing.domain.credit.entity.Credit;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CreditRepository extends JpaRepository<Credit, Long> {
-}
