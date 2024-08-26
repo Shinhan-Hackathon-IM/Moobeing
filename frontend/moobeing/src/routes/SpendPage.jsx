6508b53c7d7f95a28dc7c31264838510ef065d02
wony0321 <lyw1212@gmail.com>
2024-08-26 11:24:26 +0900

    Design: spend page finish

--- frontend/moobeing/src/routes/SpendPage.jsx
+++ frontend/moobeing/src/routes/SpendPage.jsx
@@ -5,6 +5,7 @@ import SubHeader from '../components/Spend/SubHeader';
 import PaymentHistory from '../components/Spend/PaymentHistory';
 import Calendar from '../components/Spend/Calendar';
 import PieGraph from '../components/Spend/PieGraph';
+import PaymentCategory from '../components/Spend/PaymentCategory';
 import Footer from "../components/Fixed/Footer";
 import Header from "../components/Fixed/Header";
 
@@ -24,6 +25,12 @@ const CalendarWrapper = styled.div`
   box-sizing: border-box;
 `;
 
+const PieWrapper = styled.div`
+  width: 100%;
+  padding: 20px;
+  box-sizing: border-box;
+`;
+
 const Spend = () => { 
 
   const [selectedDate, setSelectedDate] = useState(dayjs());
@@ -37,36 +44,43 @@ const Spend = () => {
     setShowAnalysis((prevShowAnalysis) => !prevShowAnalysis);
   };
 
+// 파이 데이터를 여기에 받아와서 두 컴포넌트에 다 적용하도록
   const pieData = [
     {
-      "id": "javascript",
-      "label": "javascript",
+      "id": "식비",
+      "label": "식비",
       "value": 213,
       "color": "hsl(190, 70%, 50%)"
     },
     {
-      "id": "rust",
-      "label": "rust",
+      "id": "대출",
+      "label": "대출",
       "value": 533,
       "color": "hsl(250, 70%, 50%)"
     },
     {
-      "id": "stylus",
-      "label": "stylus",
-      "value": 134,
+      "id": "문화",
+      "label": "문화",
+      "value": 100,
       "color": "hsl(234, 70%, 50%)"
     },
     {
-      "id": "sass",
-      "label": "sass",
+      "id": "유흥",
+      "label": "유흥",
       "value": 282,
       "color": "hsl(198, 70%, 50%)"
     },
     {
-      "id": "ruby",
-      "label": "ruby",
+      "id": "교통",
+      "label": "교통",
       "value": 39,
       "color": "hsl(117, 70%, 50%)"
+    },
+    {
+      "id": "건강",
+      "label": "건강",
+      "value": 34,
+      "color": "hsl(61, 96%, 81%)"
     }
   ];
 
@@ -85,7 +99,12 @@ const Spend = () => {
             <PaymentHistory date={selectedDate.format('MM월 DD일')} />
           </>
         ) : (
-          <PieGraph data={pieData} />
+          <>
+            <PieWrapper>
+              <PieGraph data={pieData} width="100%" height={300} />
+            </PieWrapper>
+            <PaymentCategory data={pieData} />
+          </>
         )}
       <Footer/>
     </Container>
