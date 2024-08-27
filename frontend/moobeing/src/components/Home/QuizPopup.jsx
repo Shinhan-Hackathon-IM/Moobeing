57e63b141ed91923d0785f66c566fecd740f327f
thunthuni1004 <thunthuni1004@gmail.com>
2024-08-27 15:45:59 +0900

    Design: home 디자인 완성

--- frontend/moobeing/src/components/Home/QuizPopup.jsx
+++ frontend/moobeing/src/components/Home/QuizPopup.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import babyRad from "../../assets/radishes/babyRad.svg";
 
 const Container = styled.div`
   background-color: #f5fded;
+  border-radius: 20px;
   height: 500px;
   width: 90%;
   margin-bottom: 5%;
@@ -15,6 +17,7 @@ const Container = styled.div`
   align-items: center;
   padding: 20px;
   box-sizing: border-box;
+  color: #24272d;
 `;
 
 const CloseButton = styled.button`
@@ -25,38 +28,44 @@ const CloseButton = styled.button`
   border: none;
   font-size: 1.5rem;
   cursor: pointer;
-  color: black;
 `;
 
 const SubTitle = styled.div`
   position: absolute;
-  top: 10px;
+  top: 15px;
   left: 20px;
-  font-size: 1.25rem;
-  font-weight: bold;
+  font-size: 1.1rem;
+  font-weight: 800;
   text-align: left;
 `;
 
 const Radish = styled.img`
   height: 150px; /* 적절한 크기로 설정 */
-  margin-bottom: 20px;
+  margin-top: 35px;
 `;
 
 const QuizButton = styled.button`
-  background-color: #4caf50;
+  background-color: #c0dda6;
   color: white;
   border: none;
   padding: 10px 20px;
-  font-size: 1rem;
+  font-size: 15px;
   cursor: pointer;
-  border-radius: 5px;
+  border-radius: 10px;
 `;
 
 function QuizPopup() {
   const [isVisible, setIsVisible] = useState(true);
+  const navigate = useNavigate(); // useNavigate 훅 사용
+
+  const quizId = null; // 실제로는 API 호출 또는 다른 방법으로 설정될 것
 
   const handleClose = () => {
     setIsVisible(false);
+  };
+
+  const handleQuiz = () => {
+    navigate(`/quiz/result/${quizId}`);
   };
 
   if (!isVisible) return null; // 컴포넌트를 숨기기
@@ -67,10 +76,10 @@ function QuizPopup() {
       <SubTitle>
         김싸피님, "내 소비내역 맞추기"
         <br />
-        퀴즈가 도착했어요
+        퀴즈가 도착했어요!
       </SubTitle>
       <Radish src={babyRad} alt="Radish" />
-      <QuizButton>퀴즈 풀고 무 뽑으러 가기</QuizButton>
+      <QuizButton onClick={handleQuiz}>퀴즈 풀고 무 뽑으러 가기</QuizButton>
     </Container>
   );
 }
