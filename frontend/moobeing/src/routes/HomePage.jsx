115bc2d9beefe8cc0f1f86eae05fb260791d264a
chwangmin <ckm7907@naver.com>
2024-08-28 14:54:22 +0900

    Hotfix: import 겹치는 부분 삭제

--- frontend/moobeing/src/routes/HomePage.jsx
+++ frontend/moobeing/src/routes/HomePage.jsx
@@ -1,7 +1,5 @@
-import React, { useState } from "react";
-import styled, { keyframes } from "styled-components";
 import React, { useState, useEffect } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import LoanHistory from "../components/Home/LoanHistory";
 import LoanPayment from "../components/Home/LoanPayment";
 import QuizPopup from "../components/Home/QuizPopup";
