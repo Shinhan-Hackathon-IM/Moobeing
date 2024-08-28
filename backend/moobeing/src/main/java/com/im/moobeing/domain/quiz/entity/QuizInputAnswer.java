805e177eadd9c9aa1ab12838e1a0d79241548825
chwangmin <ckm7907cb@gmail.com>
2024-08-28 23:29:33 +0900

    fix: NONE 정답 추가

--- backend/moobeing/src/main/java/com/im/moobeing/domain/quiz/entity/QuizInputAnswer.java
+++ backend/moobeing/src/main/java/com/im/moobeing/domain/quiz/entity/QuizInputAnswer.java
@@ -1,16 +1,17 @@
 package com.im.moobeing.domain.quiz.entity;
 
+import com.im.moobeing.global.error.ErrorCode;
+import com.im.moobeing.global.error.exception.BadRequestException;
+
 import java.util.Arrays;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import com.im.moobeing.global.error.ErrorCode;
-import com.im.moobeing.global.error.exception.BadRequestException;
-
 public enum QuizInputAnswer {
 	UP("UP"),
-	DOWN("DOWN");
+	DOWN("DOWN"),
+	NONE("NONE");
 
 	private String displayName;
 	private static final Map<String, QuizInputAnswer> convertor = Arrays.stream(QuizInputAnswer.values())
